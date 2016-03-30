2c532844634340716b0aef5205b5a9420e31889f
henry van dusen <hgv2102@columbia.edu>
2016-03-30 19:45:04 -0400

    fixed soundcloud mess

--- js/project.js
+++ js/project.js
@@ -2,10 +2,7 @@
 
 	$(document).ready(function(){
 
-		var iframeElement   = $('#frame');
-		console.log(iframeElement);
-		var widget = SC.Widget(iframeElement);
-		console.dir(widget)
+	
 
 	});
 })(jQuery);
--- page-home.php
+++ page-home.php
@@ -1,36 +1,17 @@
-<?php get_header ?>
-
-  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-  <script src="http://w.soundcloud.com/player/api.js"></script>
-  <script>
-  var total_playlists = 1;
-   $(document).ready(function() {
-     var widget = SC.Widget(document.getElementById('soundcloud_widget'));
-     widget.bind(SC.Widget.Events.READY, function() {
-       widget.toggle();
-
-     });
-     widget.bind(SC.Widget.Events.PLAY, function(){
-       var total = widget.getDuration(function(o){
-        var span = o-7200;
-        var start = Math.floor(Math.random()*span);
-        widget.seekTo(start);
-       });
-
-     });
-     $('button').click(function() {
-       widget.toggle();
-       widget.seekTo(100000);
-     });
-   });
-  </script>
+<?php get_header(); ?>
 
 
-  <iframe id="soundcloud_widget"
-      src="http://w.soundcloud.com/player/?url=https://soundcloud.com/hvandusen/sets/jammers&show_artwork=false&liking=false&sharing=false&start_track=1"
-      width="420"
-      height="120"
-      frameborder="no"></iframe>
-  <button>Play / Pause</button>
+       	 <a href='/'><h1 id='title'>as it stands . la</h1></a><br>
+	   	 <a href='/exhibitions/'><h1 id='exhibitions'>exhibitions</h1></a>
+	   	 	<div id='content'>
+		        <br><br>
+						<div class='upcoming'><?php the_field('main_text');?></div>
+						<?php $img = get_field('main_image');?>
+	       <img class="poster" src="<?php echo $img['sizes']['large']; ?>" alt='as it stands la gallery anja solonen'></img>
+	     </div>
 
-</html>
+
+
+
+
+<?php get_footer(); ?>
